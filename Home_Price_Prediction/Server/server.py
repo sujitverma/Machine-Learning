0941097fb39c18e8ed81ad66e614d8a345d652a5
sujit <sujit.cam@gmail.com>
2020-06-29 12:28:05 +0530

    Home Price Prediction Projects

--- Home_Price_Prediction/Server/server.py
+++ Home_Price_Prediction/Server/server.py
@@ -1,17 +1,8 @@
-from flask import Flask, request, jsonify
-from gevent.pywsgi import WSGIServer
+from flask import Flask, request, jsonify, render_template
 import util
 
-app = Flask(__name__)
-app.config['SERVER_NAME'] = "localhost:5000" # attempting to set explicitly, as it defaults to None 
-
-@app.route('/')
-def home():
-    return "home..."
-
-@app.route('/api', methods=['GET'])
-def index():
-    return "Hello, World!"
+util.load_saved_artifacts()
+app=Flask(__name__,template_folder='template')
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -19,7 +10,6 @@ def get_location_names():
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
@@ -28,19 +18,15 @@ def predict_home_price():
     location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
-
     response = jsonify({
         'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
-    print('server name ', app.config["SERVER_NAME"])
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    #util.load_saved_artifacts()
+    app.run()
     
-#     app.run(debug=True)
+    
